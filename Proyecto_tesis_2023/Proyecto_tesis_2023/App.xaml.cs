4b1f3f1e68862c6da4ad901b47a86967fba21e7e
Oscar <ohohurtado16@gmail.com>
2023-06-20 21:00:31 -0500

    Agregar archivos de proyecto.

---
+++ Proyecto_tesis_2023/Proyecto_tesis_2023/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Proyecto_tesis_2023.View.Tabbed;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Proyecto_tesis_2023
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new ContainerTabbedPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
