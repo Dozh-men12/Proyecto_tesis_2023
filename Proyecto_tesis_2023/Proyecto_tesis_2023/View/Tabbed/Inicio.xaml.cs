fe75aec6c27b3eae923af5f3c1169ef7b8e1c263
Oscar <ohohurtado16@gmail.com>
2023-06-25 11:42:17 -0500

    Commit Oscar (25/06/2023)

--- Proyecto_tesis_2023/Proyecto_tesis_2023/View/Tabbed/Inicio.xaml.cs
+++ Proyecto_tesis_2023/Proyecto_tesis_2023/View/Tabbed/Inicio.xaml.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using FFImageLoading.Forms;
 using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
 using Xamarin.Forms.Xaml;
 
 namespace Proyecto_tesis_2023.View.Tabbed
@@ -40,5 +41,12 @@ namespace Proyecto_tesis_2023.View.Tabbed
                 });
             }
         }
+
+        private  async void btn_dots_Clicked(object sender, System.EventArgs e)
+        {
+            await DisplayAlert("Mensaje","Mensaje de prueba","Cancelar"); 
+        }
+
+       
     }
 }
