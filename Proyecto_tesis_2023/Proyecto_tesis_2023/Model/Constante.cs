06340b937c1b8c4efc00e761df16f623c21e6774
pachasp <elovfdhfshx475@gmail.com>
2023-07-08 14:55:11 -0500

    Commit Carlos 8/07/23

---
+++ Proyecto_tesis_2023/Proyecto_tesis_2023/Model/Constante.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Proyecto_tesis_2023.Model
+{
+    public static class Constante   
+    {
+        public static string Nombre { get; set; }
+
+        public static ImageSource Imagen { get; set; }
+    }
+}
